799e68430cf8e9aa1fdd553c66ede3345476faa4
Jiaxuan Lin <mike200177@hotmail.com>
2021-01-15 03:35:02 +0800

    完成人人對戰指令

--- MultiPLayer.cpp
+++ MultiPLayer.cpp
@@ -1,5 +1,54 @@
+#include <QMouseEvent>
+
 #include "MultiPLayer.h"
+#include "MainWindow.h"
 
 void MultiPlayer::mouseReleaseEvent(QMouseEvent* ev) {
-	printf("NOTHING");
+	int clickedId = click(ev->pos());
+
+	if (ev->x() >= 1530 && ev->x() <= 1600 &&
+		ev->y() >= 0 && ev->y() <= 50) {
+		MainWindow* control = static_cast<MainWindow*>(this->parentWidget()->parentWidget());
+		control->redirectToScreen(1);
+		init();
+		shuffle();
+	}
+
+	//若點擊非法範圍 -> 無效
+	if (clickedId == -1)
+		return;
+
+	//若第一手 -> 翻棋並決定玩家棋方
+	if (turn == NULL) {
+		flipPiece(clickedId);
+		getChessTypeId(clickedId) <= 7 ? turn = BLACK : turn = RED;
+		getChessTypeId(clickedId) <= 7 ? turnConstant = BLACK : turnConstant = RED;
+		repaint();
+		return;
+	}
+
+	//選子行為，第一次選子
+	if (selectedId == -1) {
+		if (rowMask[getRowId(clickedId)] & colMask[getColumnId(clickedId)]
+			& chessType[15].pos) {
+			// 翻子
+			flipPiece(clickedId);
+			repaint();
+		}
+		else {
+			selectedId = clickedId;
+		}
+	}
+	else {
+		if (validMove(selectedId, clickedId)) {
+			movePiece(selectedId, clickedId);
+			selectedId = -1;
+			repaint();
+		}
+		else {
+			printf("Capture Failed!\n");
+		}
+		selectedId = -1;
+	}
+	repaint();
 }
